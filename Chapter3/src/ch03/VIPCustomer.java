c9200e40757ac95c43e4bccbc5f98291cd5af8aa
Ralph <punchking466@naver.com>
2022-03-21 16:36:17 +0900

    [2022.03.21] 3월 3주차

---
+++ Chapter3/src/ch03/VIPCustomer.java
@@ -0,0 +1,37 @@
+package ch03;
+
+public class VIPCustomer extends Customer{
+
+	double salesRatio;
+	private String agentID;
+	
+//	public VIPCustomer() {
+//		
+//		super(0,"no-name");
+//		bonusRatio = 0.05;
+//		salesRatio = 0.01;
+//		customerGrade = "VIP";
+//		System.out.println("VIPCustomer() call");
+//	}
+	
+	public VIPCustomer(int customerID, String customerName) {
+		super(customerID, customerName);
+		
+		bonusRatio = 0.05;
+		salesRatio = 0.01;
+		customerGrade = "VIP";
+		System.out.println("VIPCustomer(int,String) call");
+}
+
+
+	public String getAgentID() {
+		return agentID;
+	}
+
+	public void setAgentID(String agentID) {
+		this.agentID = agentID;
+	}
+	
+	
+
+}
